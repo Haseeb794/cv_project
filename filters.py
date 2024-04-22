e2beffe4057ed7bddb49703ebdcb35bfa2013288
haseeb.tahir97 <haseeb.tahir974@gmail.com>
2024-04-22 12:31:51 +0500

    added return statements to filter functions

--- filters.py
+++ filters.py
@@ -13,7 +13,7 @@ def recolorRC(src, dst):
     """
     b, g, r = cv2.split(src)
     cv2.addWeighted(b, 0.5, g, 0.5, 0, b)
-    cv2.merge((b, b, r), dst)
+    return cv2.merge((b, b, r), dst)
 
 
 def recolorRGV(src, dst):
@@ -32,7 +32,7 @@ def recolorRGV(src, dst):
     b, g, r = cv2.split(src)
     cv2.min(b, g, b)
     cv2.min(b, r, b)
-    cv2.merge((b, g, r), dst)
+    return cv2.merge((b, g, r), dst)
 
 
 def recolorCMV(src, dst):
@@ -48,4 +48,4 @@ def recolorCMV(src, dst):
     b, g, r = cv2.split(src)
     cv2.max(b, g, b)
     cv2.max(b, r, b)
-    cv2.merge((b, g, r), dst)
+    return cv2.merge((b, g, r), dst)
